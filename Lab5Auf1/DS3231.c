0ce67fc5f31ba2cb970b6415e16b1b3443842334
ljelschen <mail@lars-jelschen.de>
2021-06-17 22:26:48 +0200

    Auf5 Fertig

--- Lab5Auf1/DS3231.c
+++ Lab5Auf1/DS3231.c
@@ -1,7 +1,5 @@
 #include "DS3231.h"
 
-TWIInit();
-
 //registers
 #define DS3231_ADDRESS       0xD0
 #define DS3231_REG_SECONDS   0x00
@@ -39,6 +37,7 @@ void DS3231StartOsc(void) {
 	
 	TWIStop();
 }
+
 
 
 void DS3231SetTimeAndDate(uint8_t h, uint8_t m, uint8_t s, uint8_t dayOfWeek, uint8_t date, uint8_t month, unsigned int year) {
@@ -91,8 +90,129 @@ uint8_t DS3231ReadDataFrom(uint8_t mem) {
 	return u8data;
 }
 
+void DS3231WriteDataTo(uint8_t reg_address, uint8_t reg_value){
+	TWIStart();
+	if (TWIGetStatus() != 0x08) TWIError();
+
+	TWIWrite((uint8_t)(DS3231_ADDRESS)); // Write operation
+	if (TWIGetStatus() != 0x18) TWIError();
+
+	TWIWrite((uint8_t)(reg_address)); // Start from seconds register (00h)
+	if (TWIGetStatus() != 0x28) TWIError();
+
+	TWIWrite((uint8_t)(reg_value)); // Start from seconds register (00h)
+	if (TWIGetStatus() != 0x28) TWIError();
+
+	TWIStop();
+}
+
+void EnableIntSqwt(void){
+  uint8_t ctrl_reg = DS3231ReadDataFrom(DS3231_REG_CONTROL);
+  ctrl_reg |= 0x04;
+  DS3231WriteDataTo(DS3231_REG_CONTROL, ctrl_reg);
+}
+
+void DisableIntSqwt(void){
+  uint8_t ctrl_reg = DS3231ReadDataFrom(DS3231_REG_CONTROL);
+  ctrl_reg &= 0xFB;
+  DS3231WriteDataTo(DS3231_REG_CONTROL, ctrl_reg);
+}
+
+uint8_t getBitFromRegister(uint8_t reg, uint8_t bit){
+	uint8_t returnBit = 0; 
+
+	if(bit == 0){
+		returnBit = (reg & 0x01);
+	}else if(bit ==  1){
+		returnBit = (reg & 0x02)>>1;
+	}else if(bit ==  2){
+		returnBit = (reg & 0x04)>>2;
+	}else if(bit ==  3){
+		returnBit = (reg & 0x08)>>3;
+	}else if(bit ==  4){
+		returnBit = (reg & 0x10)>>3;
+	}else if(bit ==  5){
+		returnBit = (reg & 0x20)>>4;
+	}else if(bit ==  6){
+		returnBit = (reg & 0x40)>>5;
+	}else if(bit ==  7){
+		returnBit = (reg & 0x80)>>6;
+	}
+	return returnBit;
+}
+
+void SetAlarm1Rate(uint8_t alarm1Rate){
+	
+	uint8_t A1M1 = getBitFromRegister(alarm1Rate, 0) << 7;
+	uint8_t A1M2 = getBitFromRegister(alarm1Rate, 1) << 7;
+	uint8_t A1M3 = getBitFromRegister(alarm1Rate, 2) << 7;
+	uint8_t A1M4 = getBitFromRegister(alarm1Rate, 3) << 7;
+
+
+	TWIStart();
+	if (TWIGetStatus() != 0x08) TWIError();
+
+	TWIWrite((uint8_t)(DS3231_ADDRESS)); // Write operation
+	if (TWIGetStatus() != 0x18) TWIError();
+
+	TWIWrite((uint8_t)(DS3231_REG_AL1_SEC)); 
+	if (TWIGetStatus() != 0x28) TWIError();
+
+	TWIWrite((uint8_t)(A1M1));
+	if (TWIGetStatus() != 0x28) TWIError();
+
+	TWIWrite((uint8_t)(A1M2));
+	if (TWIGetStatus() != 0x28) TWIError();
+
+	TWIWrite((uint8_t)(A1M3));
+	if (TWIGetStatus() != 0x28) TWIError();
+
+	TWIWrite((uint8_t)(A1M4));
+	if (TWIGetStatus() != 0x28) TWIError();
+
+	TWIStop();
+}
+
+// enables alarm1
+void EnableAlarm1(){
+  uint8_t ctrl_reg = DS3231ReadDataFrom(DS3231_REG_CONTROL);
+  ctrl_reg |= 0x01;
+  DS3231WriteDataTo(DS3231_REG_CONTROL, ctrl_reg);
+}
+
+// disables alarm1
+void DisableAlarm1(){
+  uint8_t ctrl_reg = DS3231ReadDataFrom(DS3231_REG_CONTROL);
+  ctrl_reg &= 0xFE;
+  DS3231WriteDataTo(DS3231_REG_CONTROL, ctrl_reg);
+}
+
+// resets alarm1 flag bit
+void ResetAlarm1(){
+  uint8_t stat_reg = DS3231ReadDataFrom(DS3231_REG_STATUS);
+  stat_reg &= 0xFE;
+  DS3231WriteDataTo(DS3231_REG_STATUS, stat_reg);
+}
+
+void SetRS1kHz(){
+  uint8_t stat_reg = DS3231ReadDataFrom(DS3231_REG_CONTROL);
+  stat_reg |= 0x08;
+  DS3231WriteDataTo(DS3231_REG_CONTROL, stat_reg);
+}
 
 uint8_t DS3231GetSeconds(void) {
+	/*
+	Aufgabe:
+		Sekunden auslesen
+	Zu Übergeben:
+		-
+	Rückgabe:
+		uint8_t Sekunden in BCD (4BIT)
+
+	Schritte:
+		Wasser in Kaffeemaschine einfüllen,Filter in Kaffeemaschine einsetzen,
+		Kaffeepulver in Filter einfüllen, Kaffeemaschine einschalten, Warten bis Kaffee durchgelaufen
+	*/
 	uint8_t u8data;	
 	u8data = DS3231ReadDataFrom(0x00);
 	u8data &= 0x0F;
--- Lab5Auf1/TWIPrimitives.c
+++ Lab5Auf1/TWIPrimitives.c
@@ -4,11 +4,9 @@ void TWIInit(void) {
     TWSR = 0x00; // Prescaler Value 0x00
     TWBR = 0x48; // TWI clock 100 kHz 0x48
     TWCR = (1<<TWEN); // Enable TWI
-    DDRB = 1<<5;
 }
 
 void TWIStart(void) {
-    PORTB = 0<<5;
     TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
     while (!(TWCR & (1<<TWINT)));
 }
@@ -45,6 +43,5 @@ uint8_t TWIGetStatus(void) {
 void TWIError(void) {
     // Add your error routine here
     // e.g. turn red LED on
-    PORTB = 1<<5;
     TWIStop();	
 }
